65a6149ad227fc2c3473d262c5813f78abd31b78
Abu-Taher01 <nameoa01@gmail.com>
2025-12-17 23:45:53 +0600

    Enhance list copying demonstration in Python with additional examples

--- day04/micro_challenge.py
+++ day04/micro_challenge.py
@@ -1,7 +1,30 @@
+# Wrong way of copying a list in Python:
+
 a = [1,2,3]
+print(f'Initial a={a}')
+# output: Initial a=[1, 2, 3]
 
 b = a
 
 b[0] = 99
 
-print(a)
+print(f'a={a}, b={b}')
+# output: a=[99, 2, 3]
+
+#Fix: 
+# 1) list.copy()
+
+c = a.copy()
+
+c[0] = 23
+
+print(f'a={a}, c={c}')
+# output: a=[99, 2, 3], c=[23, 2, 3]
+
+# 2) list slicing
+d = a[:]
+
+d[0] = 45
+
+print(f'a={a}, d={d}')
+# output: a=[99, 2, 3], d=[45, 2, 3]
