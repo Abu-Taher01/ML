3414f50585846ec0bcf5d5e1b2182b2be30d3599
Abu-Taher01 <nameoa01@gmail.com>
2025-12-18 23:59:08 +0600

    modified the codes for more clarity

--- day04/micro_challenge/The_Reference_Trap.py
+++ day04/micro_challenge/The_Reference_Trap.py
@@ -1,30 +1,25 @@
-# Wrong way of copying a list in Python:
+# Goal : Create a List a = [1,2,3]. Set b = a. 
+#        Change the first item to 99. print a.
 
+# Wrong way of copying a list in Python:
 a = [1,2,3]
 print(f'Initial a={a}')
 # output: Initial a=[1, 2, 3]
-
 b = a
-
 b[0] = 99
-
 print(f'a={a}, b={b}')
-# output: a=[99, 2, 3]
+# output: a=[99, 2, 3] , b=[99, 2, 3]; here a chnaged because b is just a reference to a.
 
-#Fix: 
+#Solution : 
 # 1) list.copy()
-
-c = a.copy()
-
-c[0] = 23
-
-print(f'a={a}, c={c}')
-# output: a=[99, 2, 3], c=[23, 2, 3]
+a = [1, 2, 3]
+b = a.copy()
+b[0] = 99
+print(f'a={a}, b={b}')
+# output: a=[1, 2, 3], b=[99, 2, 3]
 
 # 2) list slicing
-d = a[:]
-
-d[0] = 45
-
-print(f'a={a}, d={d}')
-# output: a=[99, 2, 3], d=[45, 2, 3]
+c = a[:]
+c[0] = 99
+print(f'a={a}, c={c}')
+# output: a=[1, 2, 3], c=[99, 2, 3]
---
+++ day04/micro_challenge/The_Slicing_Surgon.py
@@ -0,0 +1,8 @@
+# Goal : Create a list data = [10, 20, 30, 40, 50]. 
+#        Create a new list with last 3 items in reverse order.
+
+data = [10,20,30,40,50]
+
+new_list = data[-1:-4:-1]
+
+print(new_list)
