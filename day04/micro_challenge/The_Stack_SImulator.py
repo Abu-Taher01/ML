9e39140e06f71d9d0daf66587bc10d96313c1580
Abu-Taher01 <nameoa01@gmail.com>
2025-12-19 00:05:07 +0600

    Initial commit

---
+++ day04/micro_challenge/The_Stack_SImulator.py
@@ -0,0 +1,12 @@
+# Goal: Create an empty list. Add number 1,2,3.
+#       then remove the last number 3 and print it.
+
+lst = []
+
+lst.append(1)
+lst.append(2)
+lst.append(3)
+
+lst.pop()
+
+print(lst)
