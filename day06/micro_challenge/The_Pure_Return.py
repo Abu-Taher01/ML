02c9e5fdd874d4150efe909c0a1a22c2fd5cb5a6
Abu-Taher01 <nameoa01@gmail.com>
2025-12-21 00:02:27 +0600

    upload new codes

---
+++ day06/micro_challenge/The_Pure_Return.py
@@ -0,0 +1,14 @@
+# Goal : Write a function add(a,b) that prints the sum but return nothing.
+#        assign the result to a variable res = add(5,5).And print res.
+
+def add(a,b):
+    print(a+b)
+
+res = add(5,5)
+
+print(res)
+
+# Explanation:
+# Every functions return something in Python.
+# If there is no return statement, the function returns None by default.
+# Best practice function should calculate and return values.
