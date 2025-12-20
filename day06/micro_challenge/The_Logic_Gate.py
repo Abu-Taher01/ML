ba00caee8e5cfd027d955c5478034a143479a7e2
Abu-Taher01 <nameoa01@gmail.com>
2025-12-21 00:32:27 +0600

    upload new codes

---
+++ day06/micro_challenge/The_Logic_Gate.py
@@ -0,0 +1,13 @@
+# Goal: Write a function is_even(num) that returns True if even, otherwise False.
+
+def is_even(num):
+    return num%2 == 0
+
+print(is_even(4))  # True
+print(is_even(7))  # False
+
+# Explanation:
+# The function is_even(num) checks if the number is even by using the modulus operator.
+# If num % 2 equals 0, the number is even, and the function returns True.
+# Otherwise, it returns False.
+ 
