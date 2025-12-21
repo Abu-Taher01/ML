e97d806bea92838873613b66f0e9207ac66d701f
Abu-Taher01 <nameoa01@gmail.com>
2025-12-22 00:14:11 +0600

    updating the codes

--- day07/micro_challenge/The_Input_gaurd.py
+++ day07/micro_challenge/The_Input_gaurd.py
@@ -3,17 +3,19 @@
 
 # Constraint : Use try/except ValueError.
 
-age = input("Please enter your age: ")
+# age = input("Please enter your age: ")
 
-try:
-    age_int = int(age)
-    print(f"Your age is {age_int}.")
-except ValueError:
-    print("Number only")
+while True:
+    try:
+        age_int = int(input("Please enter your age: "))
+        print(f"Your age is {age_int}.")
+        break
+    except ValueError:
+        print("Number only")
 
 # Explanation:
 # We ask the user to input their age.
 # We use a try block to convert the input to an integer.
-# If the conversion is successful, we print the age.
+# If the conversion is successful, we print the age and break the loop.
 # If a ValueError occurs (e.g., the user inputs text instead of a number),
-# we catch the exception in the except block and print "Number only".
+# we catch the exception in the except block and print "Number only". And the loop continues again.
