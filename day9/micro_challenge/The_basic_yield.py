aec8d73afc335c00bb787d515c54f86b5ca03144
Abu-Taher01 <nameoa01@gmail.com>
2026-01-09 19:55:13 +0600

    day9

---
+++ day9/micro_challenge/The_basic_yield.py
@@ -0,0 +1,15 @@
+# Goal: Write a function gen(n) that yields 1, then 2, then 3.loop through it.
+
+# Deep dive: Unlike return, yield pauses the function and saves the 
+#            Stake Frame(local Varibales) in RAM.
+#            The function is frozen util you call it again.
+
+
+def gen(n):
+    for i in range(1,n+1):
+        yield i
+
+g = gen(10)
+
+for i in g:
+    print(i)
