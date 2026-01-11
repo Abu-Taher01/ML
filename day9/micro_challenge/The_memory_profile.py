b39b16e9f706a22acad3cedbf07292affc1e885d
Abu-Taher01 <nameoa01@gmail.com>
2026-01-11 23:59:59 +0600

    Add initial day10

---
+++ day9/micro_challenge/The_memory_profile.py
@@ -0,0 +1,3 @@
+# Goal: Compare sys.getsize() of a list comprehension vs a generator expression 
+#       for 1 million numbers. 
+# Deep dive: A list comprehension builds the entire list in memory,
