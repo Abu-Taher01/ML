098d2d4f994d73a46cc8d642ddc0e066d4458649
Abu-Taher01 <nameoa01@gmail.com>
2026-01-16 22:27:41 +0600

    codes

--- day10/micro_challenge/10_10_Decorators_with_Arguments.py
+++ day10/micro_challenge/10_10_Decorators_with_Arguments.py
@@ -3,3 +3,19 @@
 #            1. The factory(accepts 'times').
 #            2. The decorator (accepts 'func').
 #            3. Th Wrapper(accepts 'args'). 
+
+
+def repeat(times):
+    def deco(func):
+        def wrap():
+            for _ in range(times):
+                print(func())
+            # return func()
+        return wrap
+    return deco
+
+@repeat(3)
+def greet():
+    return f"Greet the world!"
+
+greet()
